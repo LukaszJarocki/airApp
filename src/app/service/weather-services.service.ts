8853bf061d530acb8b03d3313a8ffe7b4f4391a1
LukaszJarocki <93542313+LukaszJarocki@users.noreply.github.com>
2022-07-11 12:57:40 +0200

    Update app

--- src/app/service/weather-services.service.ts
+++ src/app/service/weather-services.service.ts
@@ -5,22 +5,26 @@ import { Injectable } from '@angular/core';
 import { WeatherData } from '../models/weather.module';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherServicesService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-
-  getWeatherData(cityName: string): Observable<WeatherData>{
-   return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
       headers: new HttpHeaders()
-      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
-      .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+        .set(
+          environment.XRapidAPIHostHeaderName,
+          environment.XRapidAPIHostHeaderValue
+        )
+        .set(
+          environment.XRapidAPIKeyHeaderName,
+          environment.XRapidAPIKeyHeaderValue
+        ),
       params: new HttpParams()
-      .set('q', cityName)
-      .set('units', 'metric')
-      .set('mode', 'json')
-    })
+        .set('q', cityName)
+        .set('units', 'metric')
+        .set('mode', 'json'),
+    });
   }
 }
